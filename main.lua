81cdd9fa6bda4b4b77285f59f57d09ac0aa19436
DataSoong <34357897+DataSoong@users.noreply.github.com>
2017-12-08 15:36:36 -0200

    Add files via upload

--- main.lua
+++ main.lua
@@ -102,9 +102,8 @@ function love.update(dt)
 						local r = math.sqrt((v[1] - w[1])^2 + (v[2] - w[2])^2)
 						local a = math.atan2(v[1] - w[1], v[2] - w[2])
 						if r ~= 0 then
-							local vx = math.sin(a) / r / w[5]
-							local vy = math.cos(a) / r / w[5]
-							
+							local vx = math.sin(a) / w[5] * 10 / r
+							local vy = math.cos(a) / w[5] * 10 / r						
 							v[3] = v[3] - vx
 							v[4] = v[4] - vy
 							
